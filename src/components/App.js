4611670ada8c45b3415b55d95978570d0508e4b8
simba <simba3310@gmail.com>
2019-05-06 01:38:49 -0500

    SURR+ APP
    Changes :
    
    *Designed Login Page Component
    *Login page CSS
    
    Why:
    
    * Needed before buyers/sellers can do transactions
    * Give page unique design
    
    
    These changes addresses the need by:
    
    *Author - www.github.com/maccauhuru

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "../css/App.css";
 import Nav from "./Nav";
-import AddSurplus from "./AddSurplus"
+// import AddSurplus from "./AddSurplus"
+import Login from "./Login"
 
 const App = () => {
     return (
         <div className="App">
             <Nav />
-            <AddSurplus />
+            <Login />
         </div>
     );
 };
